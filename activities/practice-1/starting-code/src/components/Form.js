b75e60dff89c457372b9756e8d8a5249d96859ea
Kamel CHATTI <chattikamel@gmail.com>
2023-04-10 22:01:07 +0000

    solution using Ref

--- activities/practice-1/starting-code/src/components/Form.js
+++ activities/practice-1/starting-code/src/components/Form.js
@@ -1,17 +1,25 @@
+import { useRef } from "react";
+
 function Form() {
+
+  const programSelectRef = useRef();
+  const nameInputRef = useRef();
+
   function formSubmitHandler(event) {
     event.preventDefault();
+    console.log(programSelectRef.current.value);
+    console.log(nameInputRef.current.value);
   }
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" />
+        <input type="text" id="name" ref={nameInputRef}/>
       </div>
       <div className="form-control">
         <label htmlFor="program">Choose your program</label>
-        <select id="program">
+        <select id="program" ref={programSelectRef}>
           <option value="basics">The Basics</option>
           <option value="advanced">Advanced Concepts</option>
           <option value="mastery">Mastery</option>
